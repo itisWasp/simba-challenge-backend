17ca66e92c83dab53b9d078dcca1ff30285e3b73
itisWasp <mugishaisrael18@gmail.com>
2022-02-10 19:34:06 +0200

    Fetching all the transactions from the database

--- src/routes/TransactionRoute.js
+++ src/routes/TransactionRoute.js
@@ -4,5 +4,6 @@ import TransactionController from '../controllers/TransactionController';
 import privateRoute from '../middlewares/verifyToken.js';
 
 router.post('/transaction', privateRoute.authUser ,TransactionController.send);
+router.get('/history', privateRoute.authUser ,TransactionController.GetAllTransactions)
 
 export default router;
